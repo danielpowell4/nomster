4d49de4d94cffb9b7d09bf9010b56a99bc658136
Dan <danielpowell12@msn.com>
2016-07-25 13:09:56 -0600

    validation of name, address and description in places model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,16 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true,
+    length: {
+      maximum: 76,
+      minimum: 3,
+      too_long: "%{count} characters is the maximum number of characters allowed",
+      too_short: "Must be at least %{count} characters long" }
+  validates :address, presence: true
+  validates :description, presence: true
+  length: {
+    maximum: 1000,
+    minimum: 3,
+    too_long: "%{count} characters is the maximum number of characters allowed",
+    too_short: "Must be at least %{count} characters long" }
 end
